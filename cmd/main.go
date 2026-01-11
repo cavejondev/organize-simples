2f8efb136b4ac0f52fd9f2999ce450895253d912
cavejondev <gabriel.rc.dev.2006@gmail.com>
2026-01-11 01:47:44 -0300

    chore: add depencidas e main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cavejondev/organize-simples/internal/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Ola universo, eu voltei")
+	// Carrega .env
+	if err := godotenv.Load(); err != nil {
+		log.Println("arquivo .env não encontrado, usando variáveis do sistema")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := chi.NewRouter()
+
+	// Middlewares básicos
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Rotas
+	r.Post("/login", handlers.Login)
+
+	log.Println("Servidor rodando na porta", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,11 @@ toolchain go1.24.11
 require github.com/jackc/pgx/v5 v5.8.0
 
 require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
+
+require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
