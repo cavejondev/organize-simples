5b11ef168b9f45a9ce7770ab5f5d81754282f9c1
cavejondev <gabriel.rc.dev.2006@gmail.com>
2026-01-11 02:22:31 -0300

    fix: ajustado script sql inicial

--- internal/db/scripts.sql
+++ internal/db/scripts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
+    senha TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
